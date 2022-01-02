79fd6cd888c8f359218048e0aa4f38dc692e6d89
AdaltonVitor <adaltonvitor9@gmail.com>
2022-01-02 16:59:49 -0300

    exercicio de par ou impar

---
+++ ex032.py
@@ -0,0 +1,8 @@
+numero = int(input('Me diga um numero: '))
+resultado = numero % 2
+
+if resultado == 1:
+    print('O numero {} impar'.format(numero))
+
+else:
+    print('O numero {} e par' .format(numero))
