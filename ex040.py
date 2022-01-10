72b22f4f0e5b1bc011fc27c9cb889de803a97a2e
AdaltonVitor <adaltonvitor9@gmail.com>
2022-01-10 20:40:56 -0300

    qual numero e maior

---
+++ ex040.py
@@ -0,0 +1,9 @@
+n1 = int(input('digite o oprimeiro numero inteiro: '))
+n2 = int(input('digite o segudo numero inteiro ; '))
+
+if n1 > n2:
+    print('o primeiro valor e valor e maior')
+elif n1< n2:
+    print('o segundo e valor e maior ')
+else:
+    print('nao exite valor maior os dois numeros sao iguais')
